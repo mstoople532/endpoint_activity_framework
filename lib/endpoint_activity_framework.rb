f3df79b948c729ca8fd66592687097186c2b6db1
Molly Stoopler <mstoople@gmail.com>
2023-12-16 16:33:19 -0500

    sets up initial file structure

---
+++ lib/endpoint_activity_framework.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "endpoint_activity_framework/version"
+
+module EndpointActivityFramework
+  class Error < StandardError; end
+  # Your code goes here...
+end
